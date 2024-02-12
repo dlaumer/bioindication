cd6083dcd8dd3d3578a2159910f3abf8f7d1884c
Daniel Laumer <daniel.laumer@gmail.com>
2024-02-12 17:19:38 +0100

    Process panel

--- src/components/PanelProcess.tsx
+++ src/components/PanelProcess.tsx
@@ -6,10 +6,12 @@ Header.tsx
 UI for the Header, different buttons, dropdowns and titles
 
 */
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import React, { FC } from 'react';
 import { getTranslation } from '../services/languageHelper';
+import Button from './Button';
+import { selectIsLoggedIn } from '@store/selectors';
 
 type PanelProcessProps = {
     title?: string;
@@ -20,14 +22,43 @@ const PanelProcess: FC<PanelProcessProps & React.ComponentProps<'div'>> = ({
     active = false,
 }) => {
     const dispatch = useDispatch();
-    //let title = getTranslation(titleId);
+    const text = getTranslation('processInfoText');
+    const isLoggedIn = useSelector(selectIsLoggedIn);
 
     // UI part
     return (
         <div
             id={title}
             className={`flex flex-col flex-none justify-between z-30 w-full  h-full bg-white p-[5px]`}
-        ></div>
+        >
+            <div id="processInfoText" className="p-2">
+                {text}
+            </div>
+            <div className="p-2 flex flex-col items-center">
+                <div className="w-[40%] m-2">
+                    <Button
+                        titleKey="openMapViewer"
+                        onClick={() => {
+                            const url = isLoggedIn
+                                ? 'https://globe-swiss.maps.arcgis.com/apps/mapviewer/index.html?layers=665046b6489f4feaa1e25b379cb3f70c'
+                                : 'https://globe-swiss.maps.arcgis.com/apps/mapviewer/index.html?layers=014ebd4120354d9bb3795be9276b40b9';
+                            window.open(url, '_blank');
+                        }}
+                    ></Button>
+                </div>
+                <div className="w-[4 0%] m-2">
+                    <Button
+                        titleKey="openSceneViewer"
+                        onClick={() => {
+                            const url = isLoggedIn
+                                ? 'https://globe-swiss.maps.arcgis.com/home/webscene/viewer.html?layers=665046b6489f4feaa1e25b379cb3f70c'
+                                : 'https://globe-swiss.maps.arcgis.com/home/webscene/viewer.html?layers=014ebd4120354d9bb3795be9276b40b9';
+                            window.open(url, '_blank');
+                        }}
+                    ></Button>
+                </div>
+            </div>
+        </div>
     );
 };
 
